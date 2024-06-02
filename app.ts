41758eacf6c4b1d6da8551f20fff2d16d7517089
Hardart <kaino@bk.ru>
2024-06-02 18:37:17 +0300

    add origin key to cors

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static('assets'))
 app.use(cookerParser())
-app.use(cors({ credentials: true }))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use('/api/v1', routerV1)
 
 app.use('*', otherRoutes)
