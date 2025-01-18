778a3fb8828192a683f700a55ed0e1baf069cd9b
Hardart <kaino@bk.ru>
2025-01-18 20:06:22 +0300

    refactor: add dev and prod url's

--- app.ts
+++ app.ts
@@ -7,10 +7,12 @@ import otherRoutes from './handlers/routes-handler'
 
 const app = express()
 
+const allowedOrigin = process.env.NODE_ENV === 'production' ? 'https://radioshtani.ru' : 'http://localhost:3000'
+
 app.use(express.json())
 app.use(express.static('assets'))
 app.use(cookerParser())
-app.use(cors({ credentials: true, origin: 'https://radioshtani.ru' }))
+app.use(cors({ credentials: true, origin: allowedOrigin }))
 app.use('/api/v1', routerV1)
 
 app.use('*', otherRoutes)
